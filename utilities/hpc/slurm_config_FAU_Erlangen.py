bcc27573c99880e9bb572b5009424238ca22a1a0
joveh <johannes.veh@fau.de>
2021-01-28 10:04:21 +0100

    Update slurm_config_FAU_Erlangen.py
    update slurm_config

--- utilities/hpc/slurm_config_FAU_Erlangen.py
+++ utilities/hpc/slurm_config_FAU_Erlangen.py
@@ -4,13 +4,13 @@
   Please make your changes here.
   """
 
-port = 8786
+#port = 8786
 cores = 1                                   # One grid point per core, do not change
 processes = 20                                # grid points submitted in one sbatch script
 memory = '30GB'                               # memory per processes in GB
-project = 'Peru'                         # equivalent to slurm parameter --account
-name = 'Peru',                                # equivalent to slurm parameter --job-name
+project = 'my-project'                         # equivalent to slurm parameter --account
+name = 'my-job-name',                                # equivalent to slurm parameter --job-name
 queue = 'work'
 nodes = 1                                   # processes multiplied by the number of workers
-shebang = '/bin/bash -l'
+shebang = '#!/bin/bash -l'
 slurm_parameters = ['--nodes=1','--error=slurm.err','--output=slurm.out','--time=00:10:00', '-p work']
